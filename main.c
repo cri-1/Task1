f17e20da71abd3d56e4208422d4d34ebdd8e28dd
cri-1 <naz.cristian15@gmail.com>
2026-05-10 23:49:39 +0300

    Task1

--- main.c
+++ main.c
@@ -1,64 +1,19 @@
+#include "task1.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "header3.h"
 
-int main(int argc,char** argv) {
-    if (argc<3){
+int main(int argc, char *argv[]) {
+    
+    if (argc < 2) {
         return 1;
     }
-    FILE *f1=fopen(argv[1],"r");
-    FILE *f2 = fopen(argv[2],"w");
-    if (!f1||!f2){
-        return 1;
-    }
-    char n[NUM][MAX_SYM], buf[2048];
-    double p[NUM][100];
-    int d=0,x,y,flag=1;
 
-    if (fgets(buf, sizeof(buf), f1)){
-        char* t=strtok(buf,", \t\r\n");
-        x=0;
-        while (t&&x<NUM){ 
-            strncpy(n[x++], t,MAX_SYM); 
-            t=strtok(NULL,", \t\r\n"); 
-        }
+    int N = 0;
+    Node* portofoliu=fisier(argv[1], &N);
+    if (portofoliu==NULL) {
+        return 0; 
     }
-    while (fgets(buf,sizeof(buf), f1)){
-        char* t=strtok(buf,", \t\r\n");
-        y=0;
-        while (t&&y<NUM){ 
-            p[y++][d]=atof(t); 
-            t=strtok(NULL,", \t\r\n"); 
-        }
-        d++;
-    }
-
-    TreeNode* root=newT(0);
-    for (x=0;x<NUM;x++) ins(root,n[x],p[x],d);
-    for (x=0;x<NUM;x++) {
-        TreeNode* m=find(root,p[x],d);
-        if (m&&m->stocks){
-            StockList* y_curr=m->stocks;
-            while (y_curr){
-                int k=-1;
-                for(int i=0;i<NUM;i++){
-                    if(!strcmp(n[i],y_curr->symbol)){ 
-                        k=i; 
-                        break; 
-                    }
-                }
-                if (k>x){
-                    if (!flag) fprintf(f2,"\n");
-                    fprintf(f2,"%s-%s",n[x],y_curr->symbol);
-                    flag=0;
-                }
-                y_curr=y_curr->next;
-            }
-        }
-    }
-    clear(root); 
-    fclose(f1); 
-    fclose(f2);
+    double miu=0,x=0,y=0;
+    printf("%.3f\n%.3f\n%.3f\n",trunchiere(miu),trunchiere(x),trunchiere(y));
+    free_list(portofoliu);
     return 0;
 }
